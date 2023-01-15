f4c95b6432a0f42ce65c70c6757b8d3863ab4410
Anmol Srivastava <102894901+Anmol-Srv@users.noreply.github.com>
2023-01-15 17:16:48 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
-import '../../assets/styles/base.scss';
-import '../../assets/styles/Loader.scss';
-import '../../assets/styles/Landing.scss';
-import '../../assets/styles/HeaderLinks.scss';
-import '../../assets/styles/About.scss';
-import '../../assets/styles/Projects.scss';
-import '../../assets/styles/ContactForm.scss';
+import '../../assets/css/base.scss';
+import '../../assets/css/Loader.scss';
+import '../../assets/css/Landing.scss';
+import '../../assets/css/HeaderLinks.scss';
+import '../../assets/css/About.scss';
+import '../../assets/css/Projects.scss';
+import '../../assets/css/ContactForm.scss';
