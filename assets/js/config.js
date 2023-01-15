0e62324b31e338fcdab1dcdbb74add356b3a7527
Anmol Srivastava <102894901+Anmol-Srv@users.noreply.github.com>
2023-01-15 17:19:34 +0530

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,7 +0,0 @@
-import '../../assets/css/base.scss';
-import '../../assets/css/Loader.scss';
-import '../../assets/css/Landing.scss';
-import '../../assets/css/HeaderLinks.scss';
-import '../../assets/css/About.scss';
-import '../../assets/css/Projects.scss';
-import '../../assets/css/ContactForm.scss';
