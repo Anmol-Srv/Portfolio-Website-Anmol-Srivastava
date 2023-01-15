e372b20a442742ada5c7ad04df76b6f825eb5c75
Anmol Srivastava <102894901+Anmol-Srv@users.noreply.github.com>
2023-01-15 16:48:10 +0530

    Delete testing/src directory

--- testing/src/assets/js/config.js
+++
@@ -1,7 +0,0 @@
-import '../../assets/styles/base.scss';
-import '../../assets/styles/Loader.scss';
-import '../../assets/styles/Landing.scss';
-import '../../assets/styles/HeaderLinks.scss';
-import '../../assets/styles/About.scss';
-import '../../assets/styles/Projects.scss';
-import '../../assets/styles/ContactForm.scss';
